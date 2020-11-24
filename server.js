5f8cda54969def63ced7e909d9a5111e3b46946a
Jonathan Simmons <jonathansimmons@awhere.com>
2020-11-23 23:30:43 -0600

    Changed dist path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/jds-me"));
+app.use(express.static(__dirname + "/dist"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/jds-me/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 app.listen(8080);
