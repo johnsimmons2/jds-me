f38842e96fec57db756f4bc42c453f4a653508da
Jonathan Simmons <jonathansimmons@awhere.com>
2020-11-26 15:16:59 -0600

    Fixed server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/jds-me'));
+app.use(express.static(path.join(__dirname + "/dist/jds-me")));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/jds-me/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/jds-me/index.html"));
 });
 
 app.listen(4200);
